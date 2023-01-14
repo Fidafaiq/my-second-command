d748ff6293e89e89313127e25e25785bd4af932c
Fidafaiq <Fidafaiq14@gamil.com>
2023-01-13 19:25:08 -0500

    v2

--- src/Class08.java
+++ src/Class08.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Class08 {
-    public static void main(String[] args) {
-         
+    public static <scanner> void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("are you tired");
+
+         boolean tired=scan.nextBoolean();
+
+         while(!tired){
+             System.out.println("let's play soccer");
+             System.out.println("it's raining");
+             System.out.println("I'm glad i met youim");
+         }
+
+
+
     }
-}
+    }
+
---
+++ src/Class09.java
@@ -0,0 +1,14 @@
+public class Class09 {
+    public static void main(String[] args) {
+
+        int i=1;
+        while(i<=12){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("-------------");
+        for(i=1; i<=12; i++){
+            System.out.println(i);
+        }
+    }
+}
